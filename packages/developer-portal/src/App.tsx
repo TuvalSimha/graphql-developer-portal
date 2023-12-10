5b286b27e14f35cab79304e9c877627c174b0681
TuvalSimha <tuval.simha@gmail.com>
2023-12-10 18:34:26 +0200

    ts config update

--- packages/developer-portal/src/App.tsx
+++ packages/developer-portal/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import introspection from "../../developer-portal/src/examples/my-schema.introspection.json";
 import { GraphQLDeveloperPortal } from "./graphql-developer-portal";
-import React from "react";
 
 function App() {
   return <GraphQLDeveloperPortal schema={introspection} />;
