7aad6b759c8bb7cd64a15fb44469def684fad64e
TuvalSimha <tuval.simha@gmail.com>
2024-02-14 17:14:15 +0200

    Fix setup

--- packages/storybook-react/src/stories/Button.stories.js
+++ packages/storybook-react/src/stories/Button.stories.js
@@ -1,15 +1,12 @@
-import { GraphQLDeveloperPortalDisplay } from "./GraphQLDeveloperPortalDisplay";
+import GraphqlDeveloperPortal from "graphql-developer-portal";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: "GraphQL Developer Portal Display",
-  component: GraphQLDeveloperPortalDisplay,
+  title: "GraphQL Developer Portal",
+  component: GraphqlDeveloperPortal,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
-  },
-  StyleSheet: {
-    backgroundColor: "#446f1f",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
@@ -19,10 +16,4 @@ export default {
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const GraphQLDeveloperPortalDisplayMain = {
-  args: {
-    primary: true,
-    label: "GraphQL Developer Portal Display",
-  },
-};
+export const Default = () => <GraphqlDeveloperPortal foo="Test for Storybook!" />;
