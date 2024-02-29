7b4b88da1399920fb2d28cd24a6b15c93ae683be
TuvalSimha <tuval.simha@gmail.com>
2024-02-29 18:02:15 +0200

    fix some

--- packages/nextra-theme/pages/enums/_meta.ts
+++ packages/nextra-theme/pages/enums/_meta.ts
@@ -5,7 +5,5 @@ const enumsPaths = introspection.__schema.types
   .filter((type) => type.kind === "ENUM")
   .map((enumType) => `${enumType.name.toLowerCase()}`)
   .map((enumType) => enumType.replace("__", ""));
-
-console.log("enumsPaths --->", enumsPaths);
-
+  
 export default () => createCatchAllMeta(enumsPaths);
